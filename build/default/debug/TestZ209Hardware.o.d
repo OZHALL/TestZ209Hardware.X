9e5635361c5ddba9fcd44c991b97bae263fa0b5b
O.Z.Hall <o.z.hall@att.net>
2018-05-02 08:23:52 -0500

    remove working directories
    Don't need to have the working directories in the repository

--- build/default/debug/TestZ209Hardware.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/TestZ209Hardware.o:  \
-	C:/Users/OZHall/MPLABXProjects/TestZ209Hardware.X/TestZ209Hardware.asm  \
-
--- build/default/production/TestZ209Hardware.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/TestZ209Hardware.o:  \
-	C:/Users/OZHall/MPLABXProjects/TestZ209Hardware.X/TestZ209Hardware.asm  \
-
